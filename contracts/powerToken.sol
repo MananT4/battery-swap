8c80d237a4bd866b8385a907cf996d1469749536
MananT4 <thakkar.manan@yahoo.co.in>
2024-03-28 17:11:05 +0000

    Included openzeppelin

--- contracts/powerToken.sol
+++ contracts/powerToken.sol
@@ -2,8 +2,8 @@
 pragma solidity =0.8.19;
 
 
-import "/Users/Manan/node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "/Users/Manan/node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract powerToken is ERC20, Ownable {
     constructor() ERC20("PowerToken", "PWR") {
