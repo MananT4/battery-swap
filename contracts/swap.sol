20d5ef9a8dec419827ebc4c58fc6d755ae3c7098
MananT4 <thakkar.manan@yahoo.co.in>
2024-03-28 16:00:37 +0000

    Added comments

--- contracts/swap.sol
+++ contracts/swap.sol
@@ -1,7 +1,31 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity =0.8.19;
+
+/*Function Paramter Variables: _variableName
+*Everything else in camelCase: variableName,  functionName, contractName
+*Two blank lines before contracts
+*One blank line before functions
+*/
+
+//Use web3.js functions to interact with the contract: getAccount, getBalane, currentProvider, getBlock, getTransaction
+
+
+/*Order Layout for sol file:
+*SPDX-License-Identifier
+*pragma solidity
+*import statements
+*interfaces
+*libraries
+*contracts
+*/
 
 contract swap {
-  constructor() public {
-  }
+  /*Order Layout for contract:
+  *Type declarations
+  *State variables
+  *Events
+  *Errors
+  *Modifiers
+  *Methods
+  */
 }
