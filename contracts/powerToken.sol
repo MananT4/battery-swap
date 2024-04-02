f65dd3509106e9a892991282094dba810e75af06
MananT4 <thakkar.manan@yahoo.co.in>
2024-04-02 14:34:45 +0100

    Added mint, burn functions

--- contracts/powerToken.sol
+++ contracts/powerToken.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: MIT
-pragma solidity =0.8.19;
+pragma solidity 0.8.19;
 
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
@@ -7,12 +7,15 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract powerToken is ERC20, Ownable {
     constructor(address initialOwner) ERC20("PowerToken", "PWR") {
-        ERC20("PowerToken", "PWR");
         Ownable(initialOwner);
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) public {
+        _burn(from, amount);
     }
 }
     
